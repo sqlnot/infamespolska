2df4324a3fa5c946adb609ebc077e7129d0fbc8b
sqlnot <damianlis004@gmail.com>
2024-11-08 23:26:43 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,26 +145,6 @@ function openStream(username) {
     iframe.src = `https://player.twitch.tv/?channel=${username}&parent=infamespl.live&autoplay=true&allowfullscreen=true` ;
     chatIframe.src = `https://www.twitch.tv/embed/${username}/chat?parent=infamespl.live&darkpopout`;
 }
-    // Tworzymy przycisk do pełnoekranowego otwarcia streama
-    const fullscreenButton = document.createElement('button');
-    fullscreenButton.textContent = "Pełny ekran";
-    fullscreenButton.className = "fullscreen-button";
-    document.getElementById('streamViewer').appendChild(fullscreenButton);
-
-    // Funkcja przełączająca stream na pełny ekran
-    fullscreenButton.onclick = () => {
-        const player = iframe; // Przechodzimy do pełnoekranowego trybu na iframe
-        if (player.requestFullscreen) {
-            player.requestFullscreen(); // Włączamy pełny ekran
-        } else if (player.mozRequestFullScreen) { // Firefox
-            player.mozRequestFullScreen();
-        } else if (player.webkitRequestFullscreen) { // Chrome, Safari, Opera
-            player.webkitRequestFullscreen();
-        } else if (player.msRequestFullscreen) { // Internet Explorer
-            player.msRequestFullscreen();
-        }
-    };
-
 
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
